6c657c481531c6816854f2f98573c7ba443bdb9b
ZG297 <zeghaznaviz@hotmail.com>
2026-05-05 06:51:54 +0500

    Added Book and Transaction classes

---
+++ src/main/java/library/management/system/Book.java
@@ -0,0 +1,78 @@
+package library.management.system.model;
+
+public class Book {
+
+    private int bookId;
+    private String title;
+    private String author;
+    private String category;
+    private int totalCopies;
+    private int availableCopies;
+
+    public Book(int bookId, String title, String author, String category, int totalCopies, int availableCopies) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.totalCopies = totalCopies;
+        this.availableCopies = availableCopies;
+    }
+
+    
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getTotalCopies() {
+        return totalCopies;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setTotalCopies(int totalCopies) {
+        this.totalCopies = totalCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    
+    public void issueBook() {
+        if (availableCopies > 0) {
+            availableCopies--;
+        }
+    }
+
+    public void returnBook() {
+        if (availableCopies < totalCopies) {
+            availableCopies++;
+        }
+    }
+}
---
+++ src/main/java/library/management/system/Transaction.java
@@ -0,0 +1,82 @@
+package library.management.system.model;
+import java.util.Date;
+
+public class Transaction {
+
+    private int transactionId;
+    private int userId;
+    private int bookId;
+    private Date issueDate;
+    private Date dueDate;
+    private Date returnDate;
+    private double fineAmount;
+    private String status;
+
+    
+    public Transaction(int transactionId, int userId, int bookId,
+                       Date issueDate, Date dueDate,
+                       Date returnDate, double fineAmount, String status) {
+
+        this.transactionId = transactionId;
+        this.userId = userId;
+        this.bookId = bookId;
+        this.issueDate = issueDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+        this.fineAmount = fineAmount;
+        this.status = status;
+    }
+
+    
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public Date getIssueDate() {
+        return issueDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public double getFineAmount() {
+        return fineAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public void setFineAmount(double fineAmount) {
+        this.fineAmount = fineAmount;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+    public void markAsReturned(Date returnDate, double fine) {
+        this.returnDate = returnDate;
+        this.fineAmount = fine;
+        this.status = "RETURNED";
+    }
+}
